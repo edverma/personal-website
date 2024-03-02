f4cf1b948e80140f15e7bf585d1399daa091bafd
Evan Verma <edverma@icloud.com>
2024-03-02 15:52:22 -0500

    white background

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,11 +3,6 @@ import "../app.css";
 import Header from "../components/Header.svelte";
 </script>
 
-<style lang="postcss">
-	:global(body) {
-		@apply bg-pink-50;
-	}
-</style>
 <div class="flex">
 <div class="hidden md:block md:w-1/4"></div>
 <div class="p-4 w-full md:w-1/2">
