e9a05419c6e061e2f3b32ad4d7741823d8fcfead
Evan Verma <edverma@icloud.com>
2024-03-02 17:52:32 -0500

    mailgun functions

---
+++ src/lib/server/mailgun.js
@@ -0,0 +1,30 @@
+import { PERSONAL_SITE_MAILGUN, PERSONAL_SITE_DOMAIN_MAILGUN, PERSONAL_SITE_USERNAME_MAILGUN } from '$env/static/private';
+import formData from 'form-data';
+import Mailgun from 'mailgun.js';
+
+const mailgun = new Mailgun(formData);
+const client = mailgun.client({username: PERSONAL_SITE_USERNAME_MAILGUN, key: PERSONAL_SITE_MAILGUN});
+
+export const sendNewsletter = (subject, body) => {
+	client.messages.create(PERSONAL_SITE_DOMAIN_MAILGUN, {
+		from: '<newsletter@evanverma.com>',
+		to: 'newsletter@m.evanverma.com',
+		subject: subject,
+		html: body
+	})
+		.then((res) => {
+			console.log(res);
+		})
+		.catch((err) => {
+			console.error(err);
+		});
+}
+
+export const addMember = (name, email) => {
+	client.lists.create({
+		address: email,
+		name: name
+	})
+		.then(data => console.log(data))
+		.catch(err => console.error(err));
+}
