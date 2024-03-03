df4d19ef46661bc8d43e6a950b6508b5073c4570
Evan Verma <edverma@icloud.com>
2024-03-02 21:14:13 -0500

    authn

--- src/routes/posts/[slug]/+page.server.js
+++ src/routes/posts/[slug]/+page.server.js
@@ -1,11 +1,19 @@
 import {sendNewsletter} from "$lib/server/mailgun.js";
+import { SECRET } from '$env/static/private';
 
 /** @type {import('./$types').Actions} */
 export const actions = {
-	default: async ({request}) => {
+	sendNewsletter: async ({request}) => {
+		const data = await request.formData();
+		sendNewsletter(data.get('subject'), data.get('body'));
+	},
+	authn: async({request}) => {
 		const data = await request.formData();
 
-		// TODO: add auth
-		//  sendNewsletter(data.get('subject'), data.get('body'));
+		const reqSecret = data.get('secret')
+		if (reqSecret === SECRET) {
+			return {authn: true};
+		}
+		return {authn: false};
 	}
 };
--- src/routes/posts/[slug]/+page.svelte
+++ src/routes/posts/[slug]/+page.svelte
@@ -1,7 +1,10 @@
 <script>
 	import {page} from "$app/stores";
 	import snarkdown from "snarkdown";
+	import {browser} from "$app/environment";
+	import {writable} from "svelte/store";
 
+	export let form;
 	export let data;
 	const text = snarkdown(data.text);
 
@@ -13,17 +16,33 @@
 		data.append('subject', `Weekly Newsletter - ${$page.params.slug}`);
 		data.append('body', emailText);
 
-		fetch(`/posts/${$page.params.slug}`, {
+		fetch(`/posts/${$page.params.slug}?/sendNewsletter`, {
 			method: 'POST',
 			body: data
 		})
 	}
+
+	let authn = false;
+	if (browser) {
+		const localSecretStore = writable(localStorage.getItem('secret'));
+		localSecretStore.subscribe(async value => {
+			let data = new FormData();
+			data.append('secret', value)
+			const res = await fetch(`/posts/${$page.params.slug}?/authn`, {
+				method: 'POST',
+				body: data
+			})
+			const readRes = await res.body.getReader().read()
+			authn = JSON.parse(JSON.parse(new TextDecoder().decode(readRes.value)).data)[1]
+		});
+	}
 </script>
 
-// TODO: add subscribe form
-<div class="hidden text-right pb-4">
-	<button class="text-blue-400" on:click={sendNewsletter}>Send Newsletter</button>
-</div>
+{#if authn === true}
+	<div class="text-right pb-4">
+		<button class="text-blue-400" on:click={sendNewsletter}>Send Newsletter</button>
+	</div>
+{/if}
 
 <div>
 	{@html text}
