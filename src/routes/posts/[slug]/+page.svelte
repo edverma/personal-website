414ab8051021afa67cc88caa916ce0c3bbd519c0
Evan Verma <edverma@icloud.com>
2024-03-02 20:17:51 -0500

    hide send newsletter button

--- src/routes/posts/[slug]/+page.svelte
+++ src/routes/posts/[slug]/+page.svelte
@@ -20,7 +20,8 @@
 	}
 </script>
 
-<div class="text-right pb-4">
+// TODO: add subscribe form
+<div class="hidden text-right pb-4">
 	<button class="text-blue-400" on:click={sendNewsletter}>Send Newsletter</button>
 </div>
 
