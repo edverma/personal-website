420102568630439c7b6c9cdb5c4e93cbc38b5332
Evan Verma <edverma@icloud.com>
2024-03-04 10:04:08 -0500

    new post

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,10 +8,10 @@ inject({ mode: dev ? 'development' : 'production' });
 </script>
 
 <div class="flex">
-<div class="hidden md:block md:w-1/4"></div>
-<div class="p-4 w-full md:w-1/2">
-	<Header/>
-	<slot/>
-</div>
-<div class="hidden md:block md:w-1/4"></div>
+	<div class="hidden md:block md:w-1/4"></div>
+	<div class="p-4 w-full md:w-1/2">
+		<Header/>
+		<slot/>
 	</div>
+	<div class="hidden md:block md:w-1/4"></div>
+</div>
