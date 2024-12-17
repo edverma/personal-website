567e6b5493ab01fd9028c1a98a608cd1f305a4ba
Evan Verma <edverma@icloud.com>
2024-12-17 15:32:17 -0500

    Add title and meta description

--- src/routes/[post]/+page.svelte
+++ src/routes/[post]/+page.svelte
@@ -6,4 +6,9 @@
     let source = data.post.content;
 </script>
 
+<svelte:head>
+    <title>{data.post.title}</title>
+    <meta name="description" content={data.post.description} />
+</svelte:head>
+
 <SvelteMarkdown {source} renderers={{ image: MarkdownImageRenderer }} />
