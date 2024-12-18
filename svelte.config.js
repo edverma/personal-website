8539661601ff046d761ed2a5655eac95b570f895
Evan Verma <edverma@icloud.com>
2024-12-18 14:23:29 -0500

    Optimize marked dep with vite

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,13 @@
 import adapter from '@sveltejs/adapter-node';
 
+/** @type {import('@sveltejs/kit').Config} */
 export default {
   kit: {
     adapter: adapter(),
   },
+  vite: {
+    optimizeDeps: {
+      include: ['marked']
+    }
+  }
 };
