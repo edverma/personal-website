3018bad1cb5e3aa235be51b1c341bca71e483be1
Evan Verma <edverma@icloud.com>
2024-12-18 18:13:10 -0500

    log requested secret

--- src/routes/admin/+page.server.js
+++ src/routes/admin/+page.server.js
@@ -12,6 +12,7 @@ export const actions = {
 		const data = await request.formData();
 
 		const reqSecret = data.get('secret');
+		console.log('requested secret: ', reqSecret);
 		if (reqSecret === SECRET) {
             return {status: 200};
 		}
