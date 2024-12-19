f99f17a99969bbfca31882313233ac6dcf226de7
Evan Verma <edverma@icloud.com>
2024-12-18 19:44:27 -0500

    footer with npub

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,3 @@
+<div class="text-center my-12 text-gray-500">
+    <p class="text-xs"><a href="https://primal.net/edverma" target="_blank" rel="noopener noreferrer">npub18hwy8hmnwk2t66qj3mxjqkkekcwk2p0hde5h6fm74zujtyvxk95qd6xcwr</a></p>
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script>
 import "../app.css";
 import Header from "$lib/components/Header.svelte";
+import Footer from "$lib/components/Footer.svelte";
 import {dev} from "$app/environment";
 </script>
 
@@ -13,9 +14,15 @@ import {dev} from "$app/environment";
 </head>
 
 
-<div class="flex max-w-screen-xl mx-auto my-8">
+<div class="flex max-w-screen-xl mx-auto my-8 min-h-[90vh]">
 	<div class="p-4 w-11/12 md:w-9/12 mx-auto">
 		<Header/>
 		<slot/>
+
+			<div class="sticky top-[90vh]">
+				<hr/>
+				<Footer/>
+			</div>
+
 	</div>
 </div>
