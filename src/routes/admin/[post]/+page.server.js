f09573fd0b3210d4707a17da3bdd941f6bd89115
Evan Verma <edverma@icloud.com>
2024-12-23 13:43:16 -0500

    do not prerender admin

--- src/routes/admin/[post]/+page.server.js
+++ src/routes/admin/[post]/+page.server.js
@@ -1,10 +1,4 @@
-import { getPostBySlug, updatePost, setEmailSent, deletePost } from "$lib/server/db.ts";
-import { redirect } from '@sveltejs/kit';
-import { storeImages } from '$lib';
-import marked from '$lib/marked';
-import { SECRET } from '$env/static/private';
-import { sendNewsletter } from '$lib/server/mailgun.js';
-import { publishLongFormNote } from '$lib/server/nostr.js';
+import { getPostBySlug } from "$lib/server/db.ts";
 
 /** @type {import('./$types').PageServerLoad} */
 export async function load({ params }) {    
